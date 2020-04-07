bcc3c4f7f1f84b5b04cc7f4a184fabb410face66
Restyled.io <commits@restyled.io>
2020-04-07 21:40:32 +0000

    Restyled by black

--- myapp/logic.py
+++ myapp/logic.py
@@ -8,44 +8,52 @@ def playStore(url):
 
     data = bs(data, features="html.parser")
 
-    if data.title.text == 'Not Found':
-        return {'ERROR': True}
+    if data.title.text == "Not Found":
+        return {"ERROR": True}
 
     strdata = bs.prettify(data)  # making data to str type
 
-    icon = re.findall(r'<.*T75of sHb2Xb.*>', strdata)[0]
+    icon = re.findall(r"<.*T75of sHb2Xb.*>", strdata)[0]
 
     icon = re.findall(r'(?<=src=)".*?"', icon)[0][1:-1]
 
-    name = str(data.find('h1', class_='AHFaub'))
+    name = str(data.find("h1", class_="AHFaub"))
 
-    name = re.findall(r'(?<=span>).*?(?=\<)', name)[0]
+    name = re.findall(r"(?<=span>).*?(?=\<)", name)[0]
 
-    devname = str(data.find('a', class_='hrTbp R8zArc'))
+    devname = str(data.find("a", class_="hrTbp R8zArc"))
 
-    devname = re.findall(r'(?<=\>).*?(?=\<)', devname)[0]
+    devname = re.findall(r"(?<=\>).*?(?=\<)", devname)[0]
 
-    discp = str(data.find('div', jsname="sngebd"))
+    discp = str(data.find("div", jsname="sngebd"))
 
-    discp = re.sub(r'\<.*?\>', "", discp)
+    discp = re.sub(r"\<.*?\>", "", discp)
 
     if len(discp) > 200:
         discp = discp[:200] + "..."
 
-    rating = str(data.find('div', class_="BHMmbe"))
+    rating = str(data.find("div", class_="BHMmbe"))
 
-    rating = re.sub(r'\<.*?\>', '', rating)
+    rating = re.sub(r"\<.*?\>", "", rating)
 
     downloads = str(data.select(".htlgb .IQ1z0d .htlgb")[2])
 
-    downloads = re.sub(r'\<.*?\>', '', downloads)
+    downloads = re.sub(r"\<.*?\>", "", downloads)
 
     reviews = str(data.select(".AYi5wd.TBRnV")[0])
 
-    reviews = re.sub(r'\<.*?\>', '', reviews)
+    reviews = re.sub(r"\<.*?\>", "", reviews)
 
-    return {'ICON': icon, 'APP_NAME': name, 'DEV_NAME': devname, 'DISCRIPTION': discp,
-            'RATING': rating, 'DOWNLOADS': downloads, 'REVIEWS': reviews, 'ERROR': False}
+    return {
+        "ICON": icon,
+        "APP_NAME": name,
+        "DEV_NAME": devname,
+        "DISCRIPTION": discp,
+        "RATING": rating,
+        "DOWNLOADS": downloads,
+        "REVIEWS": reviews,
+        "ERROR": False,
+    }
 
 
 def appStore(url):
@@ -54,32 +62,38 @@ def appStore(url):
     data = bs(data, features="html.parser")
 
     if str(data) == "":
-        return {'ERROR': True}
+        return {"ERROR": True}
 
-    icon = data.find("picture").find('source')['srcset'].split(',')[0][:-3]
+    icon = data.find("picture").find("source")["srcset"].split(",")[0][:-3]
 
     name = str(data.select(".product-header__title.app-header__title"))
 
     name = name.split("\n")[1].strip()
 
-    devname = str(data.select(
-        ".product-header__identity.app-header__identity"))
+    devname = str(data.select(".product-header__identity.app-header__identity"))
 
     devname = devname.split("\n")[-3].strip()
 
-    discp = data.find_all("p")[1].text[:201]+"..."
+    discp = data.find_all("p")[1].text[:201] + "..."
 
     if len(discp) > 200:
         discp = discp[:200] + "..."
 
-    rating = str(
-        data.find('figcaption', class_="we-rating-count star-rating__count"))
+    rating = str(data.find("figcaption", class_="we-rating-count star-rating__count"))
 
-    rating, reviews = re.sub(r'\<.*?\>', "", rating).split(", ")
+    rating, reviews = re.sub(r"\<.*?\>", "", rating).split(", ")
 
     reviews = reviews.split()[0]
 
     downloads = "Not Available"
 
-    return {'ICON': icon, 'APP_NAME': name, 'DEV_NAME': devname, 'DISCRIPTION': discp,
-            'RATING': rating, 'DOWNLOADS': downloads, 'REVIEWS': reviews, 'ERROR': False}
+    return {
+        "ICON": icon,
+        "APP_NAME": name,
+        "DEV_NAME": devname,
+        "DISCRIPTION": discp,
+        "RATING": rating,
+        "DOWNLOADS": downloads,
+        "REVIEWS": reviews,
+        "ERROR": False,
+    }
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.appsearch, name="app_search"),
-    path('ajax/', views.ajaxsearch, name="ajax"),
+    path("", views.appsearch, name="app_search"),
+    path("ajax/", views.ajaxsearch, name="ajax"),
 ]
